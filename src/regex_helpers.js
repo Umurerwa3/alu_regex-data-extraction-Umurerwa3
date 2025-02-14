495aac8ec2df790041117281482461a0deb8064d
Umurerwa3 <o.umurerwa@alustudent.com>
2025-02-14 16:08:37 +0000

    i added regex for both phone numbers and emails

--- src/regex_helpers.js
+++ src/regex_helpers.js
@@ -2,3 +2,7 @@
 
 // Regex for Email Addresses
 const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+
+// Regex for Phone Numbers (various formats)
+const phoneRegex = /(\+?\d{1,3}[-.\s]?)?(\(?\d{3}\)?[-.\s]?)\d{3}[-.\s]?\d{4}/g;
+
