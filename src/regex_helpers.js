3cbccc99a1fd91da763056391ce9822ef8ddef13
Umurerwa3 <o.umurerwa@alustudent.com>
2025-02-14 16:05:18 +0000

    i created a file regex_helpers.js

---
+++ src/regex_helpers.js
@@ -0,0 +1,4 @@
+// Regular Expressions for each thing like email ,  contact , credit card and others
+
+// Regex for Email Addresses
+const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
